e8f860ce94bab90d071e09f1766397215f559b5b
Leo Hafdane <leo@leopc.home>
2026-02-05 14:28:48 +0100

    format + équivalent GL_NEAREST pour le sample

--- TP1/ImageBase.cpp
+++ TP1/ImageBase.cpp
@@ -34,7 +34,7 @@ ImageBase::ImageBase(int imWidth, int imHeight, bool isColor) {
         return;
 
     allocation_tableau(data, OCTET, nTaille);
-    dataD = (double*)malloc(sizeof(double) * nTaille);
+    dataD = (double *)malloc(sizeof(double) * nTaille);
     isValid = true;
 }
 
@@ -62,12 +62,12 @@ void ImageBase::reset() {
     isValid = false;
 }
 
-void ImageBase::load(const char* filename) {
+void ImageBase::load(const char *filename) {
     init();
 
     int l = strlen(filename);
 
-    if (l <= 4)  // Le fichier ne peut pas etre que ".pgm" ou ".ppm"
+    if (l <= 4) // Le fichier ne peut pas etre que ".pgm" ou ".ppm"
     {
         printf("Chargement de l'image impossible : Le nom de fichier n'est pas conforme, il doit comporter l'extension, et celle ci ne peut �tre que '.pgm' ou '.ppm'");
         exit(0);
@@ -75,74 +75,74 @@ void ImageBase::load(const char* filename) {
 
     int nbPixel = 0;
 
-    if (strcmp(filename + l - 3, "pgm") == 0)  // L'image est en niveau de gris
+    if (strcmp(filename + l - 3, "pgm") == 0) // L'image est en niveau de gris
     {
         color = false;
-        lire_nb_lignes_colonnes_image_pgm(const_cast<char*>(filename), &height, &width);
+        lire_nb_lignes_colonnes_image_pgm(const_cast<char *>(filename), &height, &width);
         nbPixel = height * width;
 
         nTaille = nbPixel;
         allocation_tableau(data, OCTET, nTaille);
-        lire_image_pgm(const_cast<char*>(filename), data, nbPixel);
-    } else if (strcmp(filename + l - 3, "ppm") == 0)  // L'image est en couleur
+        lire_image_pgm(const_cast<char *>(filename), data, nbPixel);
+    } else if (strcmp(filename + l - 3, "ppm") == 0) // L'image est en couleur
     {
         color = true;
-        lire_nb_lignes_colonnes_image_ppm(const_cast<char*>(filename), &height, &width);
+        lire_nb_lignes_colonnes_image_ppm(const_cast<char *>(filename), &height, &width);
         nbPixel = height * width;
 
         nTaille = nbPixel * 3;
         allocation_tableau(data, OCTET, nTaille);
-        lire_image_ppm(const_cast<char*>(filename), data, nbPixel);
+        lire_image_ppm(const_cast<char *>(filename), data, nbPixel);
     } else {
         printf("Chargement de l'image impossible : Le nom de fichier n'est pas conforme, il doit comporter l'extension, et celle ci ne peut �tre que .pgm ou .ppm");
         exit(0);
     }
 
-    dataD = (double*)malloc(sizeof(double) * nTaille);
+    dataD = (double *)malloc(sizeof(double) * nTaille);
 
     isValid = true;
 }
 
-bool ImageBase::save(const char* filename) {
+bool ImageBase::save(const char *filename) {
     if (!isValid) {
         printf("Sauvegarde de l'image impossible : L'image courante n'est pas valide");
         exit(0);
     }
 
     if (color)
-        ecrire_image_ppm(const_cast<char*>(filename), data, height, width);
+        ecrire_image_ppm(const_cast<char *>(filename), data, height, width);
     else
-        ecrire_image_pgm(const_cast<char*>(filename), data, height, width);
+        ecrire_image_pgm(const_cast<char *>(filename), data, height, width);
 
     return true;
 }
 
-ImageBase* ImageBase::getPlan(PLAN plan) {
+ImageBase *ImageBase::getPlan(PLAN plan) {
     if (!isValid || !color)
         return 0;
 
-    ImageBase* greyIm = new ImageBase(width, height, false);
+    ImageBase *greyIm = new ImageBase(width, height, false);
 
     switch (plan) {
-        case PLAN_R:
-            planR(greyIm->data, data, height * width);
-            break;
-        case PLAN_G:
-            planV(greyIm->data, data, height * width);
-            break;
-        case PLAN_B:
-            planB(greyIm->data, data, height * width);
-            break;
-        default:
-            printf("Il n'y a que 3 plans, les valeurs possibles ne sont donc que 'PLAN_R', 'PLAN_G', et 'PLAN_B'");
-            exit(0);
-            break;
+    case PLAN_R:
+        planR(greyIm->data, data, height * width);
+        break;
+    case PLAN_G:
+        planV(greyIm->data, data, height * width);
+        break;
+    case PLAN_B:
+        planB(greyIm->data, data, height * width);
+        break;
+    default:
+        printf("Il n'y a que 3 plans, les valeurs possibles ne sont donc que 'PLAN_R', 'PLAN_G', et 'PLAN_B'");
+        exit(0);
+        break;
     }
 
     return greyIm;
 }
 
-void ImageBase::copy(const ImageBase& copy) {
+void ImageBase::copy(const ImageBase &copy) {
     reset();
 
     isValid = false;
@@ -158,7 +158,7 @@ void ImageBase::copy(const ImageBase& copy) {
         return;
 
     allocation_tableau(data, OCTET, nTaille);
-    dataD = (double*)malloc(sizeof(double) * nTaille);
+    dataD = (double *)malloc(sizeof(double) * nTaille);
     isValid = true;
 
     for (int i = 0; i < nTaille; ++i) {
@@ -167,7 +167,7 @@ void ImageBase::copy(const ImageBase& copy) {
     }
 }
 
-unsigned char* ImageBase::operator[](int l) {
+unsigned char *ImageBase::operator[](int l) {
     if (!isValid) {
         printf("L'image courante n'est pas valide");
         exit(0);
@@ -181,7 +181,7 @@ unsigned char* ImageBase::operator[](int l) {
     return data + l * width;
 }
 
-float ImageBase::PSNR(const ImageBase& im1, const ImageBase& im2) {
+float ImageBase::PSNR(const ImageBase &im1, const ImageBase &im2) {
     double MSE = 0.0;
     for (int y = 0; y < im1.getHeight(); ++y) {
         for (int x = 0; x < im1.getWidth(); ++x) {
