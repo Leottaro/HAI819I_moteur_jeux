73a10bdc7ea1677e16293ca0275f88cf3464b995
Leottaro <leo.hafdane@gmail.com>
2026-04-23 19:52:22 +0200

    texture write

--- src/Texture.hpp
+++ src/Texture.hpp
@@ -11,6 +11,7 @@
 
 // STB
 #include <stb_image.h>
+#include <stb_image_write.h>
 
 // USUAL INCLUDES
 #include <vector>
@@ -46,6 +47,39 @@ public:
     inline const size_t getWidth() { return m_width; }
     inline const size_t getHeight() { return m_height; }
     inline const glm::u8vec4 &getPixel(size_t u, size_t v) { return m_data[v * m_width + u]; }
+
+    void savePPM(const std::string &filePath) const {
+        std::ofstream f((filePath + ".ppm").c_str(), std::ios::binary);
+        if (f.fail())
+            return;
+
+        f << "P6\n"
+          << m_width << " " << m_height << "\n255" << std::endl;
+
+        // Write pixel data
+        std::vector<char> char_data(m_data.size() * 3);
+        for (size_t i = 0; i < m_data.size(); i++) {
+            const glm::u8vec4 &color = m_data[i];
+            char_data[i * 3] = static_cast<char>(color.r);
+            char_data[i * 3 + 1] = static_cast<char>(color.g);
+            char_data[i * 3 + 2] = static_cast<char>(color.b);
+        }
+        f.write(char_data.data(), char_data.size());
+
+        f.close();
+    }
+
+    void savePNG(const std::string &filePath) const {
+        stbi_write_png((filePath + ".png").c_str(), m_width, m_height, 4, reinterpret_cast<const unsigned char *>(m_data.data()), 0);
+    }
+
+    void saveBMP(const std::string &filePath) const {
+        stbi_write_bmp((filePath + ".bmp").c_str(), m_width, m_height, 4, reinterpret_cast<const unsigned char *>(m_data.data()));
+    }
+
+    void saveTGA(const std::string &filePath) const {
+        stbi_write_tga((filePath + ".tga").c_str(), m_width, m_height, 4, reinterpret_cast<const unsigned char *>(m_data.data()));
+    }
 
     void initShaderData() {
         clearShaderData();
